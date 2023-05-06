a9c93d92c418aeb778b132e8231b8727db6f997f
Emergit2006 <127906293+Emergit2006@users.noreply.github.com>
2023-05-05 19:34:04 -0500

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -3,17 +3,15 @@ let handler  = async (m, { conn }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let texto = `*◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
 
-❕ 𝙔𝙖 𝙥𝙪𝙚𝙙𝙚 𝙞𝙣𝙨𝙩𝙖𝙡𝙖𝙧 𝙖 𝙏𝙝𝙚-𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿 ❕
+❕ 𝙔𝙖 𝙥𝙪𝙚𝙙𝙚 𝙞𝙣𝙨𝙩𝙖𝙡𝙖𝙧 𝙖 𝐂𝐇𝐀𝐑𝐋𝐈𝐄-𝐕𝟒❕
 
-*𝙑𝙄𝘿𝙀𝙊 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝘾𝙄𝙊𝙉*
-*https://youtu.be/gh5NajZOTA8*
 
 ✨ *Grupo Actualizaciones y novedades sobre el bot*
 📌 *${global.nna}*
 
 💛 *Si tienes dudas o necesitas ayuda en el proceso de la instalación puede escribir me a este número (No es Bot el número)*
 
-📲 *Wa.me/5492266466080*
+📲 *Wa.me/51994156047*
 
 ❗ *_Solo para temas de instalación_*
 
@@ -114,17 +112,14 @@ let picture = './media/menus/Menu1.jpg'
 await m.reply(`╰⊱🔰⊱ *𝑰𝒏𝒔𝒕𝒂𝒍𝒂𝒄𝒊𝒐𝒏* ⊱🔰⊱╮`)
 let instalar = `*◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
 
-💥 𝙔𝙖 𝙥𝙪𝙚𝙙𝙚 𝙞𝙣𝙨𝙩𝙖𝙡𝙖𝙧 𝙖 𝙏𝙝𝙚-𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿 ❕
-
-*𝙑𝙄𝘿𝙀𝙊 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝘾𝙄𝙊𝙉*
-*https://youtu.be/gh5NajZOTA8*
+💥 𝙔𝙖 𝙥𝙪𝙚𝙙𝙚 𝙞𝙣𝙨𝙩𝙖𝙡𝙖𝙧 𝙖 𝐂𝐇𝐀𝐑𝐋𝐈𝐄-𝐕𝟒 ❕
 
 ✨ *Si necesitas ayuda entra al grupo del bot*
 📌 *${global.nna}*
 
 💛 *Si tienes dudas o necesitas ayuda en el proceso de la instalación puede escribir me a este número (No es Bot el número)*
 
-📲 *Wa.me/5492266466080*
+📲 *Wa.me/51994156047*
 
 ❗ *_Solo para temas de instalación_*
 
@@ -157,49 +152,11 @@ ${md}
 🔸 pkg install nodejs -y
 🔸 pkg install ffmpeg -y
 🔸 pkg install imagemagick -y
-🔸 git clone https://github.com/elrebelde21/The-LoliBot-MD
-🔸 cd The-LoliBot-MD
+🔸 git clone https://github.com/Emergit2006/Carlos-MD-V3
+🔸 cd Carlos-MD-V3
 🔸 npm install
 🔸 ls
 🔸 npm start
-
-📌 *𝙊𝙋𝘾𝙄𝙊𝙉: 2 𝘼𝙍𝘾𝙃𝙄𝙑𝙊*
-*https://youtu.be/i1xjB4bmYCc*
-
-🔸 termux-setup-storage
-🔸 apt update 
-🔸 pkg upgrade 
-🔸 pkg install git -y
-🔸 pkg install nodejs -y
-🔸 pkg install ffmpeg -y
-🔸 pkg install imagemagick -y
-🔸 pkg install yarn
-🔸 cd storage/downloads/Lolibot
-🔸 ls
-🔸 npm start
-
-───────•••───────
-
-📌 𝗜𝗡𝗦𝗧𝗔𝗟𝗔𝗥 𝗘𝗡 𝗕𝗢𝗫𝗠𝗜𝗡𝗘
-
-*𝙏𝙝𝙚-𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿 : 𝘽𝙤𝙭𝙈𝙞𝙣𝙚*
-*_https://youtu.be/wWyBHtKzx9Q_*
-
-_Pagina Oficial_ 
-_https://boxmineworld.com_
-
-_Panel_
-_https://panel.boxmineworld.com_
-
-───────•••───────
-
-📌 𝗜𝗡𝗦𝗧𝗔𝗟𝗔𝗥 𝗘𝗡 𝙍𝙀𝙋𝙇𝙄𝙏 
-
-*𝙏𝙝𝙚-𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿 : 𝙍𝙀𝙋𝙇𝙄𝙏*
-https://youtu.be/SMjCcfuyWQE
-
-_Pagina Oficial_ 
-https://replit.com/
 
 ───────•••───────
 
