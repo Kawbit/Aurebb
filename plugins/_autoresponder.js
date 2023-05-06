3612a3c106b7d51485003b84c1cc4c4716aafa1a
Emergit2006 <127906293+Emergit2006@users.noreply.github.com>
2023-05-06 17:19:20 -0500

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫', body: '©Charlie', sourceUrl: `https://github.com/Emergit2006/Charlie-V4`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '𝐂𝐇𝐀𝐑𝐋𝐈𝐄-𝐕𝟒', body: '©Charlie', sourceUrl: `https://github.com/Emergit2006/Charlie-V4`, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
