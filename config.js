cf3b8d2666d9eb54bde0727b2f968fb18e248c73
Kawbit <133259319+Kawbit@users.noreply.github.com>
2023-06-02 01:34:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -148,8 +148,8 @@ global.exito = eg
 
 //⊱ ━━━━━.⋅ Información ⋅.━━━━ ⊰
 
-global.wm = '𝐂𝐇𝐀𝐑𝐋𝐈𝐄-𝐕𝟒'
-global.igfg = '𝐂𝐇𝐀𝐑𝐋𝐈𝐄-𝐕𝟒'
+global.wm = 'Aurebb'
+global.igfg = 'Aurebb'
 global.wait = '*⌛ _Cargando..._ ▬▭▭▭▭▭▭*'
 global.waitt = '*⌛ _Cargando..._ ▬▬▭▭▭*'
 global.waittt = '*⌛ _Cargando..._ ▬▬▬▬▭▭*'
